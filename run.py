907c87dbe98be134cf04a43eff5c016e3060f493
ConorM2710 <conormoyles@outlook.com>
2023-03-09 13:18:12 +0000

    define the show_board function to print it to the console

--- run.py
+++ run.py
@@ -14,4 +14,15 @@ class TicTacToe:
         """
         self.board = ['?' for i in range(9)]
 
+    def show_board(self):
+        """
+        Displays the game board
+        """
+        print(self.board[0] + " | " + self.board[1] + " | " + self.board[2])
+        print("--|--|--")
+        print(self.board[0] + " | " + self.board[1] + " | " + self.board[2])
+        print("--|--|--")
+        print(self.board[0] + " | " + self.board[1] + " | " + self.board[2])
+
+
     
